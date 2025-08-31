a8aeeb1c378f3ad333652ef55f333f2303dcd960
shiva <sivasankar407@gmail.com>
2025-08-31 08:33:23 +0530

    Commit after changes

--- src/javabasics/MethodChild.java
+++ src/javabasics/MethodChild.java
@@ -7,6 +7,9 @@ public class MethodChild
 		MethodsDemo d1= new MethodsDemo();
 		d1.myBoolen();
 		d1.myInt();
+		System.out.println("Git Demo1");
+		System.out.println("Git Demo2");
+		System.out.println("Git Demo3");
 	}
 
 }
